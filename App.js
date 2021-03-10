2f71f2bfd782710a264a0139ed4ead048531ee3e
huzaifah050 <huzaifah050@gmail.com>
2021-03-10 22:29:16 +0000

    fix: Refactoring

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from './src';
 import Button from './src/components/Button';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 
 export default function App() {
-
 	return (
 		<View style={styles.container}>
 			<WelcomeScreen />
 			<StatusBar style="auto" />
 		</View>
 	);
-
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'white',
+		backgroundColor: Colors.WHITE,
 	},
 });
