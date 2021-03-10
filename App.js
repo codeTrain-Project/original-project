6abca9f69f341d063ef655491314c7158ff9dc42
huzaifah050 <huzaifah050@gmail.com>
2021-03-10 22:31:23 +0000

    fix: refactooring

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Colors } from './src';
-import Button from './src/components/Button';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 
 export default function App() {
