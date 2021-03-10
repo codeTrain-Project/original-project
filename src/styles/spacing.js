fc336519df691867f707b652afbad7cabb2f0d6d
huzaifah050 <huzaifah050@gmail.com>
2021-03-10 18:04:57 +0000

    fix: Horizontal white space added

--- src/styles/spacing.js
+++ src/styles/spacing.js
@@ -4,7 +4,7 @@ const WINDOW_WIDTH = Dimensions.get('window').width;
 const guidelineBaseWidth = 375;
 const scaleSize = (size) => (WINDOW_WIDTH / guidelineBaseWidth) * size;
 
-export const SCALE_18 = scaleSize(18);
+export const HORIZONTAL_WHITE_SPACE = scaleSize(25);
 export const SCALE_16 = scaleSize(16);
 export const SCALE_12 = scaleSize(12);
 export const SCALE_8 = scaleSize(8);
