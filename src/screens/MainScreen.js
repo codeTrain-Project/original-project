bbc8a87852ed72e25793c905fb32186b068ba66f
huzaifah050 <huzaifah050@gmail.com>
2021-03-11 16:26:07 +0000

    faet: Main screen componenet

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from '../components/Button';
+import KeyboardComponent from '../components/KeyboardComponent';
+import Menu from '../components/Menu';
+import User from '../components/User';
+import { Spacing, Colors } from '../index';
+
+const MainScreen = () => {
+	return (
+		<View style={styles.container}>
+			<User color="white" />
+
+			<View style={styles.keyContainer}>
+				<KeyboardComponent />
+				<View style={styles.btnContainer}>
+					<Button label="Request" width="48%" btnColor="#12AA73" />
+					<Button label="Pay" width="48%" btnColor="#12AA73" />
+				</View>
+			</View>
+
+			<Menu />
+		</View>
+	);
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// marginHorizontal: Spacing.HORIZONTAL_WHITE_SPACE,
+		marginTop: 80,
+	},
+	keyContainer: {
+		marginVertical: 100,
+		// justifyContent: 'space-between',
+	},
+	btnContainer: {
+		marginHorizontal: Spacing.HORIZONTAL_WHITE_SPACE,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: 30,
+	},
+});
