23b5dd98dd39f10254fde9a4e6d37a38fb9d9cb3
Alabi Joel Nii Bortey <63015034+joel-alabi@users.noreply.github.com>
2021-03-13 03:45:15 -0800

    changed from main to welcome

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Colors } from './src';
 import MainScreen from './src/screens/MainScreen';
-
+import WelcomeScreen from './src/screens/WelcomeScreen'
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<MainScreen />
+			{/* <MainScreen /> */}
+			<WelcomeScreen/>
 			<StatusBar style="auto" />
 		</View>
 	);
