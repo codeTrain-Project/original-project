7abd7499c7a55a8dfd29edc1099c6bb47890a19c
huzaifah050 <huzaifah050@gmail.com>
2021-03-15 22:48:31 +0000

    Refactoring

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Colors } from './src';
+import AddMoneyScreen from './src/screens/AddMoneyScreen';
+import AmountCashOutScreen from './src/screens/AmountCashOutScreen';
+import FinalAddMoneyScreen from './src/screens/FinalAddMoneyScreen';
+import FinalCashOutScreen from './src/screens/FinalCashOutScreen';
 import MainScreen from './src/screens/MainScreen';
-
+import Try from './src/screens/Try';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<MainScreen />
+			<FinalCashOutScreen />
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -16,6 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: Colors.PRIMARY,
+		backgroundColor: Colors.WHITE,
 	},
 });
