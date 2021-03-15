70bc0320775cadd19fb0590f44be4480f20ccc95
huzaifah050 <huzaifah050@gmail.com>
2021-03-15 23:06:35 +0000

    chore: Reusable component

---
+++ src/screens/AmountCashOutScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import EnterAmount from '../components/EnterAmount';
+
+const AmountCashOutScreen = () => {
+	return <EnterAmount heading="Amount to Cash Out" label="Cash Out" />;
+};
+
+export default AmountCashOutScreen;
+
+const styles = StyleSheet.create({});
