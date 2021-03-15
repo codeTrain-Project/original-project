26ab1b9d30577ebbd551279f75f44c0c20cbd012
huzaifah050 <huzaifah050@gmail.com>
2021-03-15 23:06:09 +0000

    chore: reusable components

---
+++ src/screens/AddMoneyScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import EnterAmount from '../components/EnterAmount';
+
+const AddMoneyScreen = () => {
+	return <EnterAmount heading="Top up Money" label="Add" />;
+};
+
+export default AddMoneyScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/FinalCashOutScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import EnterAmountFinal from '../components/EnterAmountFinal';
+
+const FinalCashOutScreen = () => {
+	return <EnterAmountFinal label="Cash Out" />;
+};
+
+export default FinalCashOutScreen;
+
+const styles = StyleSheet.create({});
