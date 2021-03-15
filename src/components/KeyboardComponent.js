9095b55f4454136a47bca7e0924cee25749dd878
huzaifah050 <huzaifah050@gmail.com>
2021-03-15 23:05:09 +0000

    feat: Making the output text editable

--- src/components/KeyboardComponent.js
+++ src/components/KeyboardComponent.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from 'react-native';
 import VirtualKeyboard from 'react-native-virtual-keyboard';
 import { Colors } from '../index';
+import { useKeyboard } from '@react-native-community/hooks';
 
 const KeyboardComponent = ({
 	btnColor = Colors.WHITE,
@@ -12,12 +20,18 @@ const KeyboardComponent = ({
 		setText(newText);
 	};
 
-	useEffect(() => setText('0'));
 	return (
 		<View>
-			<Text style={{ ...styles.text, color: txtColor }}>{`GH₵ ${text}`}</Text>
+			<TextInput
+				autoCorrect={false}
+				style={{ ...styles.input, color: txtColor }}
+				defaultValue={`GH₵ ${text}`}
+				onSubmitEditing={Keyboard.dismiss}
+			/>
+
+			{/* <Text style={{ ...styles.text, color: txtColor }}>{`GH₵ ${text}`}</Text> */}
 			<VirtualKeyboard
-				color={Colors.WHITE}
+				color={btnColor}
 				pressMode="string"
 				onPress={(val) => changeText(val)}
 				decimal={true}
@@ -33,5 +47,11 @@ const styles = StyleSheet.create({
 		fontSize: 55,
 		textAlign: 'center',
 		marginBottom: 40,
+	},
+	input: {
+		textAlign: 'center',
+		height: 50,
+		fontSize: 45,
+		height: 100,
 	},
 });
