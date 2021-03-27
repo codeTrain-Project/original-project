475fcfd937eddb722f9ef2e2761aff51e904cc7b
huzaifah050 <huzaifah050@gmail.com>
2021-03-27 11:20:42 +0000

    chore: Firebase integration refactoring

--- MainIndex.js
+++ MainIndex.js
@@ -1,20 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import AuthNav from './src/navigation/AuthNav';
 import MainNavigator from './src/navigation/index';
 
 const MainApp = () => {
-	const [loggedIn, setLogged] = useState(true);
+	const [loggedIn, setLogged] = useState(false);
 
 	if (loggedIn) {
 		return (
 			<>
 				<MainNavigator />
+				<StatusBar style="auto" />
 			</>
 		);
 	} else {
 		return (
 			<>
 				<AuthNav />
+				<StatusBar style="auto" />
 			</>
 		);
 	}
