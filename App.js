fb3e8cec7b3eeadd35df456df0e8b9bf2f69fc8f
huzaifah050 <huzaifah050@gmail.com>
2021-03-27 11:20:02 +0000

    chore: Firebase integration refactoring

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
 import MainApp from './MainIndex';
+import firebase from './Firebase';
+import store from './src/store/index';
+import { createFirestoreInstance } from 'redux-firestore';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import AuthIsLoaded from './src/components/AuthIsLoaded';
+
+// react-redux-firebase config
+const rrfConfig = {
+	userProfile: 'users',
+	useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+};
+
+const rrfProps = {
+	firebase,
+	config: rrfConfig,
+	dispatch: store.dispatch,
+	createFirestoreInstance, // <- needed if using firestore
+};
 
 export default function App() {
-	return <MainApp />;
+	return (
+		<Provider store={store}>
+			<ReactReduxFirebaseProvider {...rrfProps}>
+				<AuthIsLoaded>
+					<MainApp />
+				</AuthIsLoaded>
+			</ReactReduxFirebaseProvider>
+		</Provider>
+	);
 }
