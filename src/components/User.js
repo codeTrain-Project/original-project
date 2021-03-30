e5b5f25d74bbde349956f1601887430bf59ca09a
huzaifah050 <huzaifah050@gmail.com>
2021-03-30 23:03:42 +0000

    fix: Making it tochable

--- src/components/User.js
+++ src/components/User.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { Spacing } from '../index';
 
-const User = ({ color, mt = 70, marginHorizontal = 0 }) => {
+const User = ({ color, mt = 70, marginHorizontal = 0, handler }) => {
 	return (
-		<View
-			style={{
-				...styles.icon,
-				marginHorizontal: marginHorizontal,
-				marginTop: mt,
-			}}
-		>
-			<FontAwesome name="user-circle-o" size={50} color={color} />
-		</View>
+		<TouchableWithoutFeedback onPress={() => handler()}>
+			<View
+				style={{
+					...styles.icon,
+					marginHorizontal: marginHorizontal,
+					marginTop: mt,
+				}}
+			>
+				<FontAwesome name="user-circle-o" size={50} color={color} />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
