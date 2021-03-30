96b6beec18c37c8633a7c327d557b32384243362
Joel Nii Bortey Alabi <joelalabi45@gmail.com>
2021-03-30 12:23:15 +0000

    added Paypage

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Colors } from './src';
-import { RequestAmount } from './src/components/RequestAmount';
-import MainNavigator from './src/navigation/index';
-import {ActivityScreen}  from './src/screens/ActivityScreen';
-import { RequestPage } from './src/screens/RequestPage';
-import Try from './src/screens/Try';
-import WelcomeScreen from './src/screens/WelcomeScreen';
+import { PayPage } from './src/screens/PayPage';
+
 
 export default function App() {
 	return (
 		<>
-			{/* <RequestPage/> */}
-			<ActivityScreen/>
+			<PayPage/>
 		</>
 	);
 }
