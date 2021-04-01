3d13f66fc36ec63693b38a126db830c5fc03915f
huzaifah050 <huzaifah050@gmail.com>
2021-04-01 21:55:02 +0000

    fix: Refactoring

--- src/screens/PayPage.js
+++ src/screens/PayPage.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { PayAmount } from '../components/PayAmount';
+import { StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
+import PayAmount from '../components/PayAmount';
 import { Colors } from '../index';
+import { transfer, test } from '../store/actions/transactionActions';
 
-const PayPage = ({ navigation }) => {
+const PayPage = ({ navigation, transfer, test }) => {
 	return (
 		<View style={styles.container}>
-			<PayAmount navigation={navigation} />
+			<PayAmount
+				navigation={navigation}
+				label="Pay"
+				action={() => transfer()}
+				transactionType="transfer"
+			/>
 		</View>
 	);
 };
 
-export default PayPage;
+const mapDispatchToProps = {
+	transfer,
+	test,
+};
+
+export default connect(null, mapDispatchToProps)(PayPage);
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
