297b5df32761249d86d0e4926ecf705725dc3130
huzaifah050 <huzaifah050@gmail.com>
2021-04-07 21:53:13 +0000

    chore: Fetching and displaying data

--- src/screens/RequestPage.js
+++ src/screens/RequestPage.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { PayAmount } from '../components/PayAmount';
-import RequestAmount from '../components/RequestAmount';
+import { connect } from 'react-redux';
+import PayAmount from '../components/PayAmount';
 import { Colors } from '../index';
+import { request, clean, all } from '../store/actions/transactionActions';
 
-const RequestPage = ({ navigation }) => {
+const RequestPage = ({ navigation, request }) => {
 	return (
 		<View style={styles.container}>
-			<PayAmount navigation={navigation} label="Request" handler = {() => console.log('Request') } />
+			<PayAmount
+				navigation={navigation}
+				label="Request"
+				transacFunc={request}
+			/>
 		</View>
 	);
 };
 
-export default RequestPage;
+const mapStateToProps = (state) => {
+	return {
+		input: state.transaction.keyboardData,
+	};
+};
+
+const mapDispatchToProps = {
+	request,
+	clean,
+	all,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(RequestPage);
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
