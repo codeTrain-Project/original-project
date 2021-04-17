1e243332b1c386919ed313ccf6f3e8069f79944a
huzaifah050 <huzaifah050@gmail.com>
2021-04-17 12:11:27 +0000

    fix: Refactoring

--- src/screens/RequestPage.js
+++ src/screens/RequestPage.js
@@ -3,32 +3,27 @@ import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import PayAmount from '../components/PayAmount';
 import { Colors } from '../index';
-import { request, clean, all } from '../store/actions/transactionActions';
+import { request } from '../store/actions/transactionActions';
 
-const RequestPage = ({ navigation, request }) => {
+const RequestPage = ({ navigation, request, route }) => {
+	const { state } = route.params;
+
 	return (
 		<View style={styles.container}>
 			<PayAmount
 				navigation={navigation}
 				label="Request"
 				transacFunc={request}
+				state={state}
 			/>
 		</View>
 	);
 };
 
-const mapStateToProps = (state) => {
-	return {
-		input: state.transaction.keyboardData,
-	};
-};
-
 const mapDispatchToProps = {
 	request,
-	clean,
-	all,
 };
-export default connect(mapStateToProps, mapDispatchToProps)(RequestPage);
+export default connect(null, mapDispatchToProps)(RequestPage);
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
