e97bfeb8b887072648c8f4dbee27a1e104893890
huzaifah050 <huzaifah050@gmail.com>
2021-04-17 12:06:20 +0000

    fix: Refactoring

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -4,9 +4,22 @@ import AddMoneyScreen from '../screens/AddMoneyScreen';
 import ModalScreen from '../screens/ModalScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import Try from '../screens/Try';
+import LinkMomo from '../screens/LinkMomo';
+import OTPScreen from '../screens/OTPScreen';
+import SuccessfulScreen from '../screens/SuccessfulScreen';
 
 const Stack = createStackNavigator();
-
+const config = {
+	animation: 'spring',
+	config: {
+		stiffness: 1000,
+		damping: 500,
+		mass: 3,
+		overshootClamping: true,
+		restDisplacementThreshold: 0.01,
+		restSpeedThreshold: 0.01,
+	},
+};
 const HomeStack = () => {
 	return (
 		<Stack.Navigator>
@@ -26,6 +39,10 @@ const HomeStack = () => {
 			/>
 			<Stack.Screen
 				options={{
+					transitionSpec: {
+						open: config,
+						close: config,
+					},
 					header: () => null,
 					headerShown: false,
 					cardStyle: { backgroundColor: 'transparent' },
@@ -57,6 +74,20 @@ const HomeStack = () => {
 				}}
 				name="Try"
 				component={Try}
+			/>
+			<Stack.Screen
+				options={{
+					header: () => null,
+				}}
+				name="LinkMomo"
+				component={LinkMomo}
+			/>
+			<Stack.Screen
+				options={{
+					header: () => null,
+				}}
+				name="OTPScreen"
+				component={OTPScreen}
 			/>
 		</Stack.Navigator>
 	);
